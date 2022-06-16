abe14648b483dded9011f3c4e9c10ecd6d77cdb8
DmitriFa <lipsuke@gmail.com>
2022-06-16 11:08:30 +0300

    16.06.22 11:07

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,96 +1,63 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
 
 import java.io.IOException;
 import java.net.URL;
 
+import static org.openqa.selenium.By.id;
+import static org.openqa.selenium.devtools.v85.dom.DOM.focus;
+
 public class Parser {
 
     public static void main(String[] args) throws Exception {
-     Elements page = getPage();
-       System.out.println(page);
-        System.setProperty("webdriver.chrome.driver","/Users/lipsuke/Downloads/Parser/.idea/selenium/chromedriver");
-      ChromeDriver webDriver = new ChromeDriver();
-        webDriver.get("https://www.glo-story.com/jackets/12979-Women-s-leather-jacket-5996525323579.html");
-     /*  for (int i = 2; i <= 7; i++) {
-            WebElement paginationBtn = webDriver.findElement(By.xpath("//*[@id=\"z0IHRoAjlM3\"]/ul/li[" + i + "]/a"));
-            paginationBtn.click();
-            Thread.sleep(1000);
-            System.out.println(i);
-        }
-        Thread.sleep(1000);
-        webDriver.quit();*/
+        System.setProperty("webdriver.chrome.driver", "/Users/lipsuke/Downloads/Parser/.idea/selenium/chromedriver");
+        String url = ("https://www.glo-story.com/login?back=my-account");
+        ChromeDriver webDriver = new ChromeDriver();
+        webDriver.get(url);
 
-
-
-       /*   int index = 0;
-        Element tableWth = page.select("table[class=wt]").first();
-        Elements names = tableWth.select("tr[class=wth]");
-        Elements values = tableWth.select("tr[valign=top]");
-
-      for(Element name: names){
-            String dateString = name.select("th[id=dt]").text();
-            String date = getDateFromString(dateString);
-            System.out.println(date + "    Явления    Температура    Давление   Влажность    Ветер");
-            int iterationCount = printPartValues(values, index);
-            index +=iterationCount;
-        }*/
+        WebElement login = webDriver.findElement(By.xpath("/html/body/div/div[2]/div/div/div/div/div/div[2]/form/div/div[1]/input"));
+        WebElement password =webDriver.findElement(By.xpath("/html/body/div/div[2]/div/div/div/div/div/div[2]/form/div/div[2]/span/input"));
+        WebElement btn =webDriver.findElement(By.xpath("/html/body/div/div[2]/div/div/div/div/div/div[2]/form/div/p[2]/button"));
+        login.sendKeys("lipsuke@gmail.com");
+        password.sendKeys("Reaba1966");
+        btn.click();
+        WebElement womans =webDriver.findElement(By.xpath("//*[@id=\"mega_menu_plus\"]/ul/li[6]/a/span"));
+        womans.click();
+        WebElement tovar =webDriver.findElement(By.cssSelector("#center_column > div > ul > li:nth-child(1) > div > div.right-block > h5 > a"));
+        WebElement tovarprice =webDriver.findElement(By.cssSelector("#center_column > div > ul > li:nth-child(1) > div > div.right-block > div.content_price > span"));
+        WebElement picture = webDriver.findElement(By.cssSelector("#center_column > div > ul > li:nth-child(1) > div > div.left-block > div > a.product_img_link.xxx > div > div.lazy.img-hover.bg-image.bg-menuproduct"));
+        System.out.println(tovar.getText()+ " price  "+tovarprice.getText());
+        picture.click();
+        WebElement photo =webDriver.findElement(By.cssSelector("#center_column > div > div > div > div.pb-left-column.col-xs-12.col-sm-6.col-md-6.col-lg-8 > div > div:nth-child(1) > a > div > img"));
+        System.out.println(photo.getAttribute("src"));
+        //String element = webDriver.findElement(By.xpath("//*[@id=\"center_column\"]/div/div/div/div[2]/div/div[2]/h1")).getText();
+       // String element1 = webDriver.findElement(By.xpath("//*[@id=\"our_price_display\"]/text()")).getText();
+       // System.out.println(womans.getText());
+        //System.out.println(getElement(url));
     }
-    public static Elements getPage() throws IOException{
-       // String url = ("http://www.pogoda.spb.ru/");
-        String url = ("https://www.glo-story.com/home/8077-women-s-leather-jacket-5996525267439.html?search_query=WPY&results=17");
+
+
+    public static void getElement(String url) throws IOException {
+     //   String url = ("https://www.glo-story.com/jackets/12979-Women-s-leather-jacket-5996525323579.html");
         Document page = Jsoup.parse(new URL(url), 6000);
-      Elements element = page.getElementsByClass("small");
-      Element el = page.getElementById("quantity_wanted");
-              {
-        }
-        return element;
+       // String element = page.getElementsByClass("small").text();
+       Elements lg = page.selectXpath("/html/body/div/div[2]/div/div/div/div/div/div[2]/form/div/div[1]/input");
+       Elements pw = page.selectXpath("/html/body/div/div[2]/div/div/div/div/div/div[2]/form/div/div[2]/span/input");
+        lg.val("lipsuke@gmail.com");
+        pw.val("Reaba1966");
     }
 
- /*   private static Pattern pattern = Pattern.compile("\\d{2}\\.\\d{2}");
 
-    private static String getDateFromString(String stringDate) throws Exception{
-        Matcher matcher =  pattern.matcher(stringDate);
-        if (matcher.find()){
-            return matcher.group();
-        }
-        throw new Exception("Can't extract date from string");
+
+    public static Document getPage(String url) throws IOException {
+     //   String url = ("https://www.glo-story.com/jackets/12979-Women-s-leather-jacket-5996525323579.html");
+        Document page = Jsoup.parse(new URL(url), 6000);
+        return page;
     }
 
-    private static int printPartValues(Elements values, int index){
-        int iterationCount = 4;
-        if (index==0){
-            Element valueLn = values.get(0);
-            /**  boolean isMorning = valueLn.text().contains("Утро");
-             if (isMorning){
-             iterationCount = 3;
-             }
-
-            switch(valueLn.text().split(" ")[0]){
-                case ("День"):
-                    iterationCount = 3;
-                    break;
-                case ("Вечер"):
-                    iterationCount = 2;
-                    break;
-                case ("Ночь"):
-                    iterationCount = 1;
-                    break;
-            }
-
-        }
-
-        for(int i=0; i<iterationCount; i++){
-            Element valueLine = values.get(index+i);
-            for(Element td: valueLine.select("td")){
-                System.out.print(td.text()+"    ");
-            }
-            System.out.println();
-        }
-        return(iterationCount);
-    }*/
-}
+    }
